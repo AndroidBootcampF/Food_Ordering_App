7c3ff83213f8f92c0314dd272b9daddf78cbb2f6
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-07-26 21:18:43 +0300

    Added All Files

--- app/src/main/java/com/pilekumatlar/foodorderingapp/fragments/lists/FragmentListRestaurant.kt
+++ app/src/main/java/com/pilekumatlar/foodorderingapp/fragments/lists/FragmentListRestaurant.kt
@@ -5,46 +5,60 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.pilekumatlar.foodorderingapp.R
 import com.pilekumatlar.foodorderingapp.databinding.FragmentListRestaurantBinding
 import com.pilekumatlar.foodorderingapp.models.restaurants
 
-class FragmentListRestaurant: Fragment(R.layout.fragment_list_restaurant) {
-    private lateinit var binding:FragmentListRestaurantBinding
-    private val adapter=ListAdapter()
+class FragmentListRestaurant : Fragment(R.layout.fragment_list_restaurant) {
+    private lateinit var binding: FragmentListRestaurantBinding
+    private val adapter = ListAdapter()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= FragmentListRestaurantBinding.bind(view)
+        binding = FragmentListRestaurantBinding.bind(view)
         initViews(view)
         getDataFromFirebase()
     }
-   private fun getDataFromFirebase(){
-        val db=FirebaseFirestore.getInstance()
+
+    private fun getDataFromFirebase() {
+        val db = FirebaseFirestore.getInstance()
         db.collection("Restaurants")
             .get()
             .addOnSuccessListener {
-                val listTwo=ArrayList<restaurants>()
-                for (i in it.documents){
-                    val restaurantInformations=i.toObject(restaurants::class.java)!!
-                    listTwo.add(restaurants(restaurantInformations.restaurantName,restaurantInformations.restaurantLocation))
+                val listTwo = ArrayList<restaurants>()
+                for (i in it.documents) {
+                    val restaurantInformations = i.toObject(restaurants::class.java)!!
+                    listTwo.add(
+                        restaurants(
+                            restaurantInformations.restaurantName,
+                            restaurantInformations.restaurantLocation
+                        )
+                    )
                 }
                 adapter.setRestaurantDataTwo(listTwo)
             }.addOnFailureListener {
-                Log.v("Hata","Hata Alındı")
-            } }
+                Log.v("Hata", "Hata Alındı")
+            }
+    }
 
-    private fun initViews(view: View){
+    private fun initViews(view: View) {
 
-        binding.restaurantsRecyclerView.layoutManager= LinearLayoutManager(context)
-        binding.restaurantsRecyclerView.adapter=adapter
-        adapter.setRestaurantOnClickListener(object :IRestaurantOnClickListener{
+        binding.restaurantsRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.restaurantsRecyclerView.adapter = adapter
+        adapter.setRestaurantOnClickListener(object : IRestaurantOnClickListener {
             override fun onClick(restaurants: restaurants) {
-                Toast.makeText(context,"${restaurants.restaurantName}",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "${restaurants.restaurantName}", Toast.LENGTH_SHORT).show()
 
             }
-        }) }
+        })
+        binding.addRestaurantButton.setOnClickListener {
+            val action =
+                FragmentListRestaurantDirections.actionFragmentListRestaurantToFragmentAddRestaurant()
+            findNavController().navigate(action)
+        }
+    }
 
 }
 
--- app/src/main/java/com/pilekumatlar/foodorderingapp/models/restaurants.kt
+++ app/src/main/java/com/pilekumatlar/foodorderingapp/models/restaurants.kt
@@ -1,10 +1,11 @@
 package com.pilekumatlar.foodorderingapp.models
 
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 
-
+@Parcelize
 data class restaurants(
-    val restaurantName:String="",
-    val restaurantLocation:String=""
-)
+    val restaurantName: String = "",
+    val restaurantLocation: String = ""
+) : Parcelable
