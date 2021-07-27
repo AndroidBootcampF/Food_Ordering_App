f2470a3b4433b136ef1774ddea61156ab7924c6d
Uğur İnci <ugurinci@Ugurs-MacBook-Pro.local>
2021-07-27 21:18:34 +0300

    format code

--- app/src/main/java/com/pilekumatlar/foodorderingapp/MainActivity.kt
+++ app/src/main/java/com/pilekumatlar/foodorderingapp/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.pilekumatlar.foodorderingapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.pilekumatlar.foodorderingapp.databinding.FragmentListRestaurantBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.pilekumatlar.foodorderingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
 }
