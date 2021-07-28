bfe3af6fdc3fed340ea6e2554173cd66fd04a274
Uğur İnci <ugur.inci.96@gmail.com>
2021-07-29 00:35:56 +0300

    firebase storage image

--- app/src/main/java/com/pilekumatlar/foodorderingapp/fragments/addingfragments/FragmentAddFood.kt
+++ app/src/main/java/com/pilekumatlar/foodorderingapp/fragments/addingfragments/FragmentAddFood.kt
@@ -2,30 +2,27 @@ package com.pilekumatlar.foodorderingapp.fragments.addingfragments
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
+import com.google.firebase.storage.FirebaseStorage
 import com.pilekumatlar.foodorderingapp.R
 import com.pilekumatlar.foodorderingapp.databinding.FragmentAddFoodItemBinding
-import com.pilekumatlar.foodorderingapp.models.FoodItem
+import java.util.*
 
 class FragmentAddFood : Fragment(R.layout.fragment_add_food_item) {
-    private val args: FragmentAddFoodArgs by navArgs()
     private var _binding: FragmentAddFoodItemBinding? = null
 
     private val binding get() = _binding!!
 
-    private val mFirestore = FirebaseFirestore.getInstance()
+    private val storage = FirebaseStorage.getInstance()
 
-    val REQUEST_CODE = 100
+    private var pickedImage: Uri? = null
+
+    private val REQUEST_CODE = 100
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,27 +35,19 @@ class FragmentAddFood : Fragment(R.layout.fragment_add_food_item) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.textAddFood.setOnClickListener {
-            val foodName: String = binding.editTextFoodName.text.toString()
-            val foodDescription: String = binding.editTextFoodDescription.text.toString()
-            val foodPrice: String = binding.editTextFoodPrice.text.toString()
-            val foodId: String = args.id.toString()
-
-            val foodInformations = FoodItem(foodName, foodDescription, foodPrice, foodId)
-
-            mFirestore.collection("Foods")
-                .document()
-                .set(foodInformations, SetOptions.merge())
-                .addOnSuccessListener {
-                    Toast.makeText(context, "Yemek Ekleme Başarılı", Toast.LENGTH_SHORT).show()
-                    val action =
-                        FragmentAddFoodDirections.actionFragmentAddFoodToFragmentListRestaurant()
-                    findNavController().navigate(action)
+            val uuid = UUID.randomUUID()
+            val imageName = "${uuid}.png"
+            val reference = storage.reference
+            val imageReference = reference.child("images").child(imageName)
+            if (pickedImage != null) {
+                imageReference.putFile(pickedImage!!).addOnSuccessListener {
+                    val uploadedImageReference =
+                        FirebaseStorage.getInstance().reference.child("images").child(imageName)
+                    uploadedImageReference.downloadUrl.addOnSuccessListener {
+                    }
                 }
-                .addOnFailureListener {
-                    Log.v("Yemek", "Ekleme Başarısız")
-                }
+            }
         }
         binding.buttonUploadImage.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
@@ -71,6 +60,9 @@ class FragmentAddFood : Fragment(R.layout.fragment_add_food_item) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
             binding.imageView.setImageURI(data?.data) // handle chosen image
+            if (data != null) {
+                pickedImage = data.data
+            }
         }
     }
 }
